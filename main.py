ec4ad25fc8d9087efd517724b5ec7b2f7dffc98c
Sophie <sophieshears01@gmail>
2021-05-05 11:34:40 -0500

    Added register, login, user page

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from markupsafe import escape
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return 'Welcome to your profile %s' % escape(username) 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
