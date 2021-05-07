864182629275fdc2a5bb4cbe3da2dab4ad703f88
Sophie <sophieshears01@gmail>
2021-05-07 14:07:05 -0500

    added css

--- setupDB.py
+++ setupDB.py
@@ -4,12 +4,11 @@ con = sqlite3.connect('users.db')
 
 cur = con.cursor()
 
-# cur.execute('''CREATE TABLE users(
-#             username VARCHAR(10),
-#             password VARCHAR(10)
-#             )''')
+cur.execute('''CREATE TABLE users(
+            username VARCHAR(10),
+            password VARCHAR(10)
+            )''')
 
-cur.execute("INSERT INTO users VALUES (:username, :password)", {'username':'test', 'password': '12345'})
 con.commit()
 
 con.close()
